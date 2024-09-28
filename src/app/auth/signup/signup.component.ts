ed0a9fe67c843175996363050bfba4a1894c575a
Shubham Vast <shubhamvast5@gmail.com>
2024-09-28 10:49:50 +0530

    Added formsArray , custom validator to formGroup so inline formControl get validated together

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
+  FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 
+// function equalValues(control:AbstractControl){
+//   const password = control.get('password')?.value;
+//   const confirmPassword = control.get('confirmPassword')?.value;
+//   if(password === confirmPassword){
+//     return null;
+//   }
+//   return {passwordNotEquals: true}
+// }
+function equalValues(password: string, confirmPassword: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(password)?.value;
+    const val2 = control.get(confirmPassword)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return { passwordNotEquals: true };
+  };
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -18,32 +38,43 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
     }),
-    passwords: new FormGroup({
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
-      }),
-    }),
+    passwords: new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+      },
+      {
+        // validators: [equalValues]
+        validators: [equalValues('password','confirmPassword')],
+      }
+    ),
     firstName: new FormControl('', { validators: [Validators.required] }),
     lastName: new FormControl('', { validators: [Validators.required] }),
-    address : new FormGroup({
+    address: new FormGroup({
       street: new FormControl('', { validators: [Validators.required] }),
       number: new FormControl('', { validators: [Validators.required] }),
       postalCode: new FormControl('', { validators: [Validators.required] }),
       city: new FormControl('', { validators: [Validators.required] }),
     }),
-    
+
     role: new FormControl<
       'student' | 'teacher' | 'employee' | 'founder' | 'other'
     >('student', { validators: [Validators.required] }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
   onSubmit() {
-    if(this.mySignUpForm.invalid){
-      console.log("INVALID FORM...!");
+    if (this.mySignUpForm.invalid) {
+      console.log('INVALID FORM...!');
     }
     console.log(this.mySignUpForm);
   }
